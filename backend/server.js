f9b629c2ccedc500385cf673392b136432e0d5ff
Chimata Veeranjaneyulu <chimataveeranjaneyulu143@gmail.com>
2025-07-17 23:45:28 +0530

    URL

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173',
+    'https://ecommerce-veeranjaneyulus-projects-9479064a.vercel.app/'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
